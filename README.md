29ee6184244e9027cae2e1506f12458feab84de3
capsule <valentin.rudloff.perso@gmail.com>
2021-08-04 07:28:46 +0000

    adapt copy to can_decoder

--- README.md
+++ README.md
@@ -1,31 +1,26 @@
-*Date of creation: 29/03/2021*
+*Date of creation: 04/08/2021*
 
-# Gps measure
-Parse the gps nmea and send via the mqtt server
+# Can decoder
+Decode CAN frame of the Capsule car (Toyota Hilux) via OBD-II interface and a python library
 
 ## Requirements
-pynmea
-serial
+python-can
+paho
+pyyaml
 
 # Installation
 Create a python virtual environnement: 'virtualenv .venv"
 Install requirements 'pip install -r requirements.txt'
 Install the configurations and service and udev rule 'python3 (from the virtualenv) install.py'
-Change default user and pass config in /etc/capsule/gps_measure/config.yaml
+Change default user and pass config in /etc/capsule/can_decoder/config.yaml
 
 ## Details
-### Programming Language
-This project is written in python and is meant to be run on a device that supports GPS queries.
-
 ### Integration
 We assume that the script will only be run on Linux OS
 TODO
 
 # Mosquitto publish topics
-process/gps_measure/alive
-gps_measure/fixed
-gps_measure/longitude
-gps_measure/latitude
-gps_measure/altitude
-gps_measure/speed
-gps_measure/route
+process/can_decoder/alive
+can_decoder/speed
+can_decoder/rpm
+can_decoder/motor_temperature
--- install.py
+++ install.py
@@ -3,13 +3,12 @@ import os
 import re
 import shutil
 
-print("Configuring gps_measure app...")
-path_to_app = "/etc/capsule/gps_measure"
-path_to_log = "/var/log/capsule/gps_measure"
-path_to_conf = "/etc/capsule/gps_measure/config.yaml"
-path_to_conf = "/etc/capsule/gps_measure/config.yaml"
-path_to_services = "/etc/systemd/system/gps_measure.service"
-path_to_udev_rules = "/etc/udev/rules.d/gps-local.rules"
+print("Configuring can_decoder app...")
+path_to_app = "/etc/capsule/can_decoder"
+path_to_log = "/var/log/capsule/can_decoder"
+path_to_conf = "/etc/capsule/can_decoder/config.yaml"
+path_to_conf = "/etc/capsule/can_decoder/config.yaml"
+path_to_services = "/etc/systemd/system/can_decoder.service"
 if not os.path.exists(path_to_app):
     print("Create folder", path_to_app)
     os.makedirs(path_to_app, 0o775)
@@ -21,27 +20,13 @@ if not os.path.exists(path_to_log):
     os.chown(path_to_log, 1000, 0) # Rudloff id and group Root
     os.chmod(path_to_log, 0o775) # Give all read access but Rudloff write access
 if not os.path.exists(path_to_conf):
-    print("Create gps_measure configuration")
+    print("Create can_decoder configuration")
     shutil.copy2(os.path.join(os.path.dirname(__file__), "default_config.yaml"), path_to_conf)
     os.chown(path_to_conf, 1000, 0) # Rudloff id and group Root
     os.chmod(path_to_conf, 0o775) # Give all read access but Rudloff write access
 if not os.path.exists(path_to_services):
-    print("Create gps_measure service")
-    shutil.copy2(os.path.join(os.path.dirname(__file__), "gps_measure.service"), path_to_services)
+    print("Create can_decoder service")
+    shutil.copy2(os.path.join(os.path.dirname(__file__), "can_decoder.service"), path_to_services)
     os.chmod(path_to_services, 0o775) # Give all read access but Rudloff write access
     os.system("systemctl daemon-reload")
-    os.system("systemctl enable gps_measure.service")
-if not os.path.exists(path_to_udev_rules):
-    print("Create udev rules")
-    with open(path_to_udev_rules, "w") as f:
-        f.write("SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"1546\", ATTRS{idProduct}==\"01a7\", SYMLINK+=\"gps_receiver\"\n")
-else:
-    print("Find devices in udev rules")
-    with open(path_to_udev_rules, "r+") as f:
-        # If we do not find the udev rules then create it
-        if not re.search("ATTRS{idVendor}==\"1546\"", f.read()):
-            print("Create udev rules")
-            f.write("SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"1546\", ATTRS{idProduct}==\"01a7\", SYMLINK+=\"gps_receiver\"\n")
-        else:
-            print("Udev rule already created")
-os.system("udevadm trigger")
+    os.system("systemctl enable can_decoder.service")
