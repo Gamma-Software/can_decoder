90685e7215c170dffc23228074c49f75738f6b6e
capsule <valentin.rudloff.perso@gmail.com>
2023-11-14 18:04:31 +0000

    up

---
+++ RESET_BLUETOOTH_CONNECT.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rfcomm release 0
+rfcomm bind 0 24:42:16:08:00:00
--- disc.py
+++ disc.py
@@ -1,13 +1,12 @@
 import obd
 
 obd.logger.setLevel(obd.logging.DEBUG) # enables all debug information
-connection = obd.OBD("/dev/rfcomm0", 38400,check_voltage=False,  start_low_power=False, fast=False)
-
+connection = obd.OBD("/dev/rfcomm0", 38400, start_low_power=False, fast=False)
 
 cmd = obd.commands.ELM_VOLTAGE # select an OBD command (sensor)
 
 response = connection.query(cmd) # send the command, and parse the response
 
-print(response.value.magnitude) # returns unit-bearing values thanks to Pint
+print(response.value) # returns unit-bearing values thanks to Pint
 
 connection.close()
